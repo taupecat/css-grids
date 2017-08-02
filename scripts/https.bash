65988dc742bcc987d1c878c200250ffcd3b99cb9
Tracy Rotton <trotton@taupecatstudios.com>
2017-08-02 08:53:56 -0400

    updated the https generation script using the personal certificate authority method from delicious brains

--- scripts/https.bash
+++ scripts/https.bash
@@ -1,5 +1,9 @@
 #/bin/bash
 
-openssl req -config /vagrant/scripts/https.conf -new -sha256 -newkey rsa:2048 \
--nodes -keyout wp-framework.local.key -x509 -days 365 \
--out wp-framework.local.crt
+SITENAME=wp-framework.local
+
+openssl genrsa -out $SITENAME.key 2048
+
+openssl req -config /vagrant/scripts/https.conf -new -key $SITENAME.key -out $SITENAME.csr
+
+openssl x509 -req -in $SITENAME.csr -CA /vagrant/scripts/myCA.pem -CAkey /vagrant/scripts/myCA.key -CAcreateserial -out $SITENAME.crt -days 365 -sha256 -extfile /vagrant/scripts/https.conf

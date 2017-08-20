71e90d4116de4ded4b44025f380ce72ae9c68933
Tracy Rotton <trotton@taupecatstudios.com>
2017-08-20 15:55:42 -0500

    moving all the variables together

--- scripts/https.bash
+++ scripts/https.bash
@@ -1,9 +1,12 @@
 #/bin/bash
 
 SITENAME="##URL##"
+PEM="##PEM##"
+KEY="##KEY##"
 
 openssl genrsa -out $SITENAME.key 2048
 
 openssl req -config /vagrant/scripts/https.conf -new -key $SITENAME.key -out $SITENAME.csr
 
-openssl x509 -req -in $SITENAME.csr -CA /vagrant/scripts/##PEM## -CAkey /vagrant/scripts/##KEY## -CAcreateserial -out $SITENAME.crt -days 365 -sha256 -extfile /vagrant/scripts/https.conf
+openssl x509 -req -in $SITENAME.csr -CA /vagrant/scripts/$PEM -CAkey /vagrant/scripts/$KEY \
+	-CAcreateserial -out $SITENAME.crt -days 365 -sha256 -extfile /vagrant/scripts/https.conf

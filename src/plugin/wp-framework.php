5e0354bd713f4ae0dfa9af1de5c77b08a8b65580
Tracy Rotton <trotton@taupecatstudios.com>
2017-09-23 16:27:08 -0400

    fixed the placeholder for the Plugin Name and adding a line to disable Jetpack nags

--- src/plugin/wp-framework.php
+++ src/plugin/wp-framework.php
@@ -1,6 +1,14 @@
 <?php
 /**
- * Plugin Name: WP Framework
+ * Plugin Name: ##PROJECT_NAME##
  * Author: Taupecat Studios
  * Author URI: https://taupecatstudios.com
  */
+
+/**
+ * Disabling Jetpack upsell messages.
+ * HT: https://mattreport.com/disable-jetpack-upsell-ads/
+ * (Requires PHP 5.5 or higher)
+ */
+
+add_filter( 'jetpack_just_in_time_msgs', function() { return false; } );

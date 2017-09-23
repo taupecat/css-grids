7069fb894ebdab86b07f8223f6d6ea255975181d
Tracy Rotton <trotton@taupecatstudios.com>
2017-09-23 18:54:32 -0400

    fixing minimum PHP requirement

--- src/plugin/wp-framework.php
+++ src/plugin/wp-framework.php
@@ -2,7 +2,7 @@
 /**
  * Disabling Jetpack upsell messages.
  * HT: https://mattreport.com/disable-jetpack-upsell-ads/
- * (Requires PHP 5.5 or higher)
+ * (Requires PHP 5.3 or higher)
  */
 
 add_filter( 'jetpack_just_in_time_msgs', function() { return false; } );
